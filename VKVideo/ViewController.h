20e82701a9007721cd0eace11c2088e1abe26c1c
alexbutenko <alexandr.butenko@gmail.com>
2014-11-24 20:08:02 +1300

    Initial Commit

---
+++ VKVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKVideo
+//
+//  Created by alexbutenko on 11/24/14.
+//  Copyright (c) 2014 alexbutenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
